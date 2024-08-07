2139d0a9ac4d3618c36f44f7030ff54fbc097093
박성언 <tjddjs909@naver.com>
2024-08-07 23:33:34 +0900

    build gradle, app property 설정 완료

---
+++ outfoot/src/main/java/com/example/outfoot/OutfootApplication.java
@@ -0,0 +1,13 @@
+package com.example.outfoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutfootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutfootApplication.class, args);
+    }
+
+}
---
+++ outfoot/src/test/java/com/example/outfoot/OutfootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.outfoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutfootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
